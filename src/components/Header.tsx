da32ffc26dc501dc1b64ab6ec3ce51b5a9cecd02
polux0 <afidelo@protonmail.com>
2025-08-11 22:29:57 +0200

    fix: same logo for all the pages!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <header className="bg-white">
       <div className="flex items-center justify-between px-6 py-4">
-        <Link to="/" className="text-xl font-bold text-contribo-text">
-          Contribo
+        <Link to="/" className="flex items-center">
+          <img src="/reputation/images/contribo.png" alt="CONTRIBO" className="h-10 w-auto" />
         </Link>
       </div>
     </header>
